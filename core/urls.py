1cb3f9b93cb160a11f1198f3b01fcd0415401173
vadimb <seowebdes@mail.ru>
2026-04-22 02:54:24 +0300

    feat: add localized index path with django-distill support

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,11 @@ from .views import index
 
 app_name = "core"
 
+
+def get_index_urls():
+    return ["en", "ru", "uk"]
+
+
 urlpatterns = [
-    distill_path("en/", index, name="index_en"),
-    distill_path("ru/", index, name="index_ru"),
-    distill_path("uk/", index, name="index_uk"),
+    distill_path("<lang>/", index, name="index", distill_func=get_index_urls),
 ]
