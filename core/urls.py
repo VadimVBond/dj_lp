a27366ad6e0a616bab074ca14026127ece57e748
vadimb <seowebdes@mail.ru>
2026-04-22 03:04:57 +0300

    Fix multilingual static generation by correctly using i18n_patterns and language activation

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,14 @@
 from django_distill import distill_path
 from .views import index
+from .distill import get_index_urls
 
 app_name = "core"
 
-
-def get_index_urls():
-    return ["en", "ru", "uk"]
-
-
 urlpatterns = [
-    distill_path("<lang>/", index, name="index", distill_func=get_index_urls),
+    distill_path(
+        "",
+        index,
+        name="index",
+        distill_func=get_index_urls
+    ),
 ]
